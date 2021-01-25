1f720d23e53d5f1b9853b13f55b44cbb0e9d93fa
intiplayer <77198908+intiplayer@users.noreply.github.com>
2021-01-25 13:10:47 +0400

    Add files via upload

---
+++ CloudUP.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const { Client, RichEmbed } = require("discord.js");
+
+const CloudUP = new Discord.Client({ disableEveryone: true, disabledEvents: ["TYPING_START"] });// Discorda bağlanırken sorun yaşamaması için :).
+
+const { CommandHandler } = require("djs-commands");// Komut Modülü
+
+require('dotenv').config();// .env içindir. Token & Database için çok önemli modüldür. Gizli kılar.
+
+const CH = new CommandHandler({
+    folder: __dirname + "/CloudUP-Commands/",
+  timeout: 1.4,
+  prefix: ["c!", "C!", "."]
+})//komutu çalıştırmak ve komutları çekmek için olan kısımdır.
+
+CloudUP.on("message", async (message) => {
+    if(message.channel.type === 'dm') return;
+    if(message.author.type === 'bot') return;
+
+    let args = message.content.split(" ");
+    let command = args[0];
+    let cmd = CH.getCommand(command);
+
+    if(!cmd) return;
+
+    try{
+
+        cmd.run(CloudUP,message,args)
+
+    }catch(e){
+        console.log(e)
+    }
+  console.log(`${message.author.tag} adlı kullanıcı ${command} adlı kodu kullandı! Komutu kullandığı kanal: ${message.channel.name} || Sunucu: ${message.guild.name}`)
+//Komutu kim kullanmış kim nerede ne yapıyor kısmı.
+});
+
+
+CloudUP.on('ready', () => {
+console.log(`${CloudUP.user.tag} Aktif halde.`)
+CloudUP.user.setStatus('online').catch(console.error)})//Profile ne koyacağını sen yap!
+
+CloudUP.login(process.env.CLIENT_TOKEN).catch(console.error);//Stabil çalışmasını sağlar.
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Cloudun
+# CloudUP Javascript Edition
+
+## Direkt Davet: [Buraya Tıkla](https://discordapp.com/oauth2/authorize?client_id=686185592899633200&scope=bot&permissions=805315704).
+`İşte CloudUP Bot için bildir! Bir sorun var ve ya talebinde bulunmaktan çekinmeyin!`
+
+`Discord.js çalışma örneği olarak sunulmuştur.  Resmi bot python yazılım dili olarak sunulmaktadır.`
+
+
+## License
+
+*Apache License*
+
+> `Aşağıdaki şartlar altında:`
+
+`-` **Açık kaynaklı kod paylaşıma açık değildir. Klonu oluşturan kişi & şahıslara gerekli İşlemler uygulanmaktadır.**
+
+
+**Daha fazla bilgi için:** [Buraya Tıkla](https://discord.gg/ja5PTUj).
+
+
+# Discord v12 Göre Editlenmiştir.
+
+`process.env.CLIENT_TOKEN` Çalıştırmak için => .env olan dosya içeriğine `CLIENT_TOKEN=TokenBuraya` gelecek şekilde koyunuz.
